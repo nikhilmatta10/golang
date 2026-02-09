d36914117f75d3549911890e207a4870aecbc687
Nikhil Matta <nikhil.matta@ascendion.com>
2026-02-09 12:38:07 +0530

    added cond signal &   broadcast example

--- sync/examples/03-cond/main.go
+++ sync/examples/03-cond/main.go
@@ -31,7 +31,7 @@ var sharedRsc = make(map[string]interface{})
 
 // }/
 
-func main() {
+func SingleWaitCond() {
 	var wg sync.WaitGroup
 
 	mu := sync.Mutex{}
@@ -65,4 +65,94 @@ func main() {
 	c.L.Unlock()
 
 	wg.Wait()
+}
+
+
+// func BroadcastExample(){
+
+// 	var wg sync.WaitGroup
+
+// 	wg.Add(1)
+
+// 	go func ()  {
+		
+// 		defer wg.Done()
+
+// 		// TODO: suspend goroutine until sharedRsc is populated.
+
+// 		for len(sharedRsc) < 1 {
+// 			time.Sleep(1 * time.Millisecond)
+// 		}
+
+// 		fmt.Println(sharedRsc["rsc1"])
+// 	}()
+
+// 	wg.Add(1)
+// 	go func ()  {
+// 		defer wg.Done()
+
+// 		for len(sharedRsc) < 2 {
+// 			time.Sleep(1 * time.Millisecond)
+// 		}
+
+// 		fmt.Println(sharedRsc["rsc2"])
+// 	}()
+
+
+// 	sharedRsc["rsc1"] = "foo"
+// 	sharedRsc["rsc2"] = "bar"
+
+// 	wg.Wait()
+// }
+
+
+func BroadcastExample(){
+
+	var wg sync.WaitGroup
+
+	mu := sync.Mutex{}
+	c := sync.NewCond(&mu)
+
+
+	wg.Add(1)
+
+	go func ()  {
+		
+		defer wg.Done()
+
+		// TODO: suspend goroutine until sharedRsc is populated.
+
+		c.L.Lock()
+		for len(sharedRsc) < 1 {
+			c.Wait()
+		}
+
+		fmt.Println(sharedRsc["rsc1"])
+		c.L.Unlock()
+	}()
+
+	wg.Add(1)
+	go func ()  {
+		defer wg.Done()
+
+		c.L.Lock()
+		for len(sharedRsc) < 2 {
+			c.Wait()
+		}
+
+		fmt.Println(sharedRsc["rsc2"])
+		c.L.Unlock()
+	}()
+
+	c.L.Lock()
+	sharedRsc["rsc1"] = "foo"
+	sharedRsc["rsc2"] = "bar"
+	c.Broadcast()
+	c.L.Unlock()
+
+	wg.Wait()
+}
+
+func main(){
+	BroadcastExample()
 }
